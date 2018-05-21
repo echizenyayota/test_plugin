8821510f0337fafbc6ee07fad1de539e772a03e4
echizenyayota <virtualmisso30@gmail.com>
2018-05-21 10:33:13 +0900

    プラグインの認識

---
+++ wp-content/plugins/mytest/mytest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package express
+ */
+/*
+Plugin Name: mytest
+Plugin URI: https://example.com
+Description: mytest
+Version: 0.0.0
+Author: echizenya
+Author URI: https://e-yota.com
+License: GPLv2 or later
+Text Domain: mytest
+*/
