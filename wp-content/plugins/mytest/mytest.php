535be48a660d7d1dc6db60821b48daf794d8211e
echizenyayota <virtualmisso30@gmail.com>
2018-05-22 12:22:28 +0900

    管理画面にプラグイン用カスタムメニューの追加

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -12,3 +12,10 @@ Author URI: https://e-yota.com
 License: GPLv2 or later
 Text Domain: mytest
 */
+
+// 管理画面にプラグイン用カスタムメニューの追加
+function register_my_custom_menu_page() {
+	add_menu_page( 'test plugin', 'test plugin', 'manage_options', 'mytest/mytest-admin.php', '', plugins_url( 'mytest/images/ecoteki.png' ), 66 );
+}
+
+add_action( 'admin_menu', 'register_my_custom_menu_page' );
