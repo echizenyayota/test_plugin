af63d389475a819d571e4a6ba77a74296d26d133
echizenyayota <virtualmisso30@gmail.com>
2018-05-22 13:29:09 +0900

    権限グループの変更

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -15,7 +15,7 @@ Text Domain: mytest
 
 // 管理画面にプラグイン用カスタムメニューの追加
 function register_my_custom_menu_page() {
-	add_menu_page( 'test plugin', 'test plugin', 'manage_options', 'mytest/mytest-admin.php', '', plugins_url( 'mytest/images/ecoteki.png' ), 66 );
+	add_menu_page( 'test plugin', 'test plugin', 'administrator', 'mytest/mytest-admin.php', '', plugins_url( 'mytest/images/ecoteki.png' ), 66 );
 }
 
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
