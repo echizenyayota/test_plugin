fe21ab5bc2e05191ea70229478423302eaf8283f
echizenyayota <virtualmisso30@gmail.com>
2018-05-27 22:05:52 +0900

    Settings API を使用したプラグインの完成

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -1,14 +1,21 @@
 <?php
-/**
- * @package express
- */
-/*
-Plugin Name: mytest
-Plugin URI: https://example.com
-Description: mytest
-Version: 0.0.0
-Author: echizenya
-Author URI: https://e-yota.com
-License: GPLv2 or later
-Text Domain: mytest
-*/
+// create custom plugin settings menu
+function baw_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('BAW Plugin Settings', 'BAW Settings', 'administrator', __FILE__, 'baw_settings_page',plugins_url('/images/icon.png', __FILE__));
+
+	//call register settings function
+	add_action( 'admin_init', 'register_mysettings' );
+}
+add_action('admin_menu', 'baw_create_menu');
+
+
+function register_mysettings() {
+	//register our settings
+	register_setting( 'baw-settings-group', 'new_option_name' );
+	register_setting( 'baw-settings-group', 'some_other_option' );
+	register_setting( 'baw-settings-group', 'option_etc' );
+}
+
+require_once(__DIR__ . '/mytest_admin.php');
