f36c2ac1b6138d3dd89cca9580c1c00d4eacb081
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 09:58:30 +0900

    セクションのタイトルを h3 タグで囲み、設定項目のフィールドをテーブルタグで囲んで出力

--- wp-content/plugins/mytest/mytest_admin.php
+++ wp-content/plugins/mytest/mytest_admin.php
@@ -4,6 +4,7 @@
     <h2>Your Plugin Page Title</h2>
     <form method="post" action="options.php">
       <?php settings_fields( 'test-settings-group' ); ?>
+      <?php do_settings_sections( 'test-settings-group' ); ?>
       <?php submit_button(); ?>
     </form>
   </div>
