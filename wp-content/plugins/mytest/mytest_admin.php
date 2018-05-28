9e29ff910a409e5aaba5a3f949c1b8ceb8155650
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 11:50:21 +0900

    options データベーステーブルから名前を指定してオプションの値を取得した値をサニタイズする

--- wp-content/plugins/mytest/mytest_admin.php
+++ wp-content/plugins/mytest/mytest_admin.php
@@ -8,15 +8,15 @@
       <table class="form-table">
         <tr valign="top">
         <th scope="row">New Option Name</th>
-        <td><input type="text" name="new_option_name" value="<?php get_option('new_option_name'); ?>" /></td>
+        <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
         </tr>
         <tr valign="top">
         <th scope="row">Some Other Option</th>
-        <td><input type="text" name="some_other_option" value="<?php get_option('some_other_option'); ?>" /></td>
+        <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
         </tr>
         <tr valign="top">
         <th scope="row">Options, Etc.</th>
-        <td><input type="text" name="option_etc" value="<?php get_option('option_etc'); ?>" /></td>
+        <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
         </tr>
       </table>
       <?php submit_button(); ?>
