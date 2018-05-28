ea855e7af4ed997ea19a5f3acc63772044466e89
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 09:34:06 +0900

    プラグイン設定ページの追加

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -12,3 +12,9 @@ Author URI: https://e-yota.com
 License: GPLv2 or later
 Text Domain: mytest
 */
+
+// 管理画面にプラグイン用カスタムメニューの追加
+function test_create_menu() {
+
+}
+add_action('admin_menu', 'test_create_menu');
