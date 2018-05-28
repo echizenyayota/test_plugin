c93eb1d952006a65141c6ab0522b00fb9efcf1b2
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 19:05:16 +0900

    プラグインの認識（記述が削除されていた）

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * @package express
+ */
+/*
+Plugin Name: mytest
+Plugin URI: https://example.com
+Description: mytest
+Version: 0.0.0
+Author: echizenya
+Author URI: https://e-yota.com
+License: GPLv2 or later
+Text Domain: mytest
+*/
+
 // create custom plugin settings menu
 function baw_create_menu() {
 
