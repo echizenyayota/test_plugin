cc158711a0fcb281e932f355e68e4a2cbc143c62
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 09:35:22 +0900

    プラグイン設定ページの追加

--- wp-content/plugins/mytest/mytest.php
+++ wp-content/plugins/mytest/mytest.php
@@ -15,6 +15,7 @@ Text Domain: mytest
 
 // 管理画面にプラグイン用カスタムメニューの追加
 function test_create_menu() {
-
+	//プラグイン設定ページの追加
+	add_menu_page('TEST Plugin Settings', 'TEST Settings', 'administrator', __FILE__, 'test_settings_page',plugins_url('/images/icon.png', __FILE__));
 }
 add_action('admin_menu', 'test_create_menu');
