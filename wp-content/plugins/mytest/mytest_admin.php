5029c759551f76058dc9781f4065d5dd0f0dc48e
echizenyayota <virtualmisso30@gmail.com>
2018-05-28 10:03:07 +0900

    tableタグとフォームのマークアップ

--- wp-content/plugins/mytest/mytest_admin.php
+++ wp-content/plugins/mytest/mytest_admin.php
@@ -5,6 +5,20 @@
     <form method="post" action="options.php">
       <?php settings_fields( 'test-settings-group' ); ?>
       <?php do_settings_sections( 'test-settings-group' ); ?>
+      <table class="form-table">
+        <tr valign="top">
+        <th scope="row">New Option Name</th>
+        <td><input type="text" name="new_option_name" value="" /></td>
+        </tr>
+        <tr valign="top">
+        <th scope="row">Some Other Option</th>
+        <td><input type="text" name="some_other_option" value="" /></td>
+        </tr>
+        <tr valign="top">
+        <th scope="row">Options, Etc.</th>
+        <td><input type="text" name="option_etc" value="" /></td>
+        </tr>
+      </table>
       <?php submit_button(); ?>
     </form>
   </div>
