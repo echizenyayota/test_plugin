5488621412bde3cd9a1a2d91f29be6fb5fe4c86b
echizenyayota <virtualmisso30@gmail.com>
2018-05-29 09:38:01 +0900

    プラグインヘッダーの記述

---
+++ wp-content/plugins/MySettingsPage/MySettingsPage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package express
+ */
+/*
+Plugin Name: MySettingsPage
+Plugin URI: https://example.com
+Description: MySettingsPage
+Version: 0.0.0
+Author: echizenya
+Author URI: https://e-yota.com
+License: GPLv2 or later
+Text Domain: mytest
+*/
