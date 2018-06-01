e1a3981dec8ed89f4955420389d4cd5f1b68afa6
echizenyayota <virtualmisso30@gmail.com>
2018-06-01 19:53:22 +0900

    設定画面を表示するメソッドの定義

--- wp-content/plugins/MySettingsPage/MySettingsPage.php
+++ wp-content/plugins/MySettingsPage/MySettingsPage.php
@@ -23,6 +23,37 @@ class MySettingsPage {
     add_action( 'admin_init', array( $this, 'page_init' ) );
   }
 
+  public function add_plugin_page() {
+    // 設定メニューページにサブメニューページを追加する
+    add_options_page(
+        'Settings Admin',
+        'My Settings',
+        'manage_options',
+        'my-setting-admin',
+        array( $this, 'create_admin_page' ) // 使用するメソッドを明示する
+    );
+  }
+
+  public function create_admin_page() {
+   // my_option_nameをoptionsのプロパティとする
+   $this->options = get_option( 'my_option_name' );
+   ?>
+   <?php var_dump($this->options); ?>
+   <div class="wrap">
+       <h2>My Settings</h2>
+       <form method="post" action="options.php">
+       <?php
+           // 設定項目の表示
+           settings_fields( 'my_option_group' );
+           do_settings_sections( 'my-setting-admin' );
+           // 保存して送信
+           submit_button();
+       ?>
+       </form>
+   </div>
+   <?php
+  }
+
 
 }
 
